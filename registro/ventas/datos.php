91ad3f67e9830a7c1606153fa61f3a193bddc061
Julio Figueroa <juliofigueroaisme@gmail.com>
2024-02-17 10:23:50 -0400

    act

--- registro/ventas/datos.php
+++ registro/ventas/datos.php
@@ -530,9 +530,9 @@ obtenerValoresDesdeDatosPHP();
 						<div class="form-group">
 							<label>Estado</label>
 							<select class="form-control modal-select" id="estado" name="estado" >
-        <option value="Venezuela">Completada</option>
-        <option value="Peru">Pendiente</option>
-             <option value="Ecuador">Por confirmar</option>
+        <option value="Completada">Completada</option>
+        <option value="Pendiente">Pendiente</option>
+             <option value="Por confirmar">Por confirmar</option>
 
 
       </select>
